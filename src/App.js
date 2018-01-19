894b2523ab18cb23e7ae03072463afe43307ad07
Vasil Genov <vasil.genov@ad.mentormate.bg>
2018-01-19 19:36:15 +0200

    a

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    value: '',
+    show: false
+  }
+  
+  submit = () => this.setState({ value: this.text.value });
+  
+  showValue = () => this.setState({ show: !this.state.show });
+
   render() {
     return (
       <div className="App">
@@ -13,6 +22,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {/* <input type="text" ref={node => this.text = node} /> */}
+        <input type="text" value={this.state.value} onChange={e => this.setState({ value: e.target.value.trim() })} />
+        {/* <button onClick={this.submit}>Show Value</button> */}
+        <button onClick={this.showValue}>Toggle value</button>
+        <p>{this.state.show && this.state.value.toLocaleUpperCase()}</p>
       </div>
     );
   }
