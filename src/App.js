57f1e9f31352037bf64f90f48f7d8a78003a7925
Vasil Genov <vasil.genov@ad.mentormate.bg>
2018-01-22 14:46:26 +0200

    itterating and child components

--- src/App.js
+++ src/App.js
@@ -2,31 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  state = {
-    value: '',
-    show: false
-  }
-  
-  submit = () => this.setState({ value: this.text.value });
-  
-  showValue = () => this.setState({ show: !this.state.show });
+import Movie from './Movie';
 
+const movies = [
+  {
+    id: 1,
+    title: 'Star Wars'
+  }, {
+    id: 2,
+    title: 'Spider Man'
+  }, {
+    id: 3,
+    title: '36th Chamber of Shaolin'
+  }
+];
+
+class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        {/* <input type="text" ref={node => this.text = node} /> */}
-        <input type="text" value={this.state.value} onChange={e => this.setState({ value: e.target.value.trim() })} />
-        {/* <button onClick={this.submit}>Show Value</button> */}
-        <button onClick={this.showValue}>Toggle value</button>
-        <p>{this.state.show && this.state.value.toLocaleUpperCase()}</p>
+        <ul>
+          {movies.map(movie => 
+            <Movie key={movie.id} title={movie.title} />)}
+        </ul>
       </div>
     );
   }
---
+++ src/Movie.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Movie = ({ title }) => <li>{title}</li>;
+
+export default Movie;
